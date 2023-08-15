6ab72012ad2bc983eeaa77a36cfc45dc7159a529
KK - SUMON <137429372+umon987@users.noreply.github.com>
2023-08-15 19:39:39 +0600

    Add files via upload

---
+++ LOVE.py
@@ -0,0 +1,40 @@
+import random
+import time
+import os
+with open (input("\33[1;92mcreate file :"),"w") as f:
+	print("\33[1;92mchack your file maneger")
+	f.write("tumi amir jan\n")
+	f.write("love you konika\n")
+	f.write("তোমাকে অনেক ভালো বাসি আমি")
+	
+print(" \33[1;91mKK KING  is back, fuck your system 🖤☠️🖤")
+time.sleep(6)
+os.system('xdg-open https://www.facebook.com/profile.php?id=100090316953207')
+os.system('clear')
+
+print("""
+╔━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+║\33[1;91m➣ DEVOLPER    :    \33[1;91mKK
+║\33[1;92m➣ FACEBOOK    :    Md Sumon islam 
+║\33[1;93m➣ TEAM        :    ELITE™
+║\33[1;94m➣ WHATSAPP    :    01725891440
+║\33[1;95m➣ GITHUB      :    SUMON987
+║\33[1;96m➣ TOOLS       :    LOVE CRACKING
+║\33[1;92m➣ VERSION     :    EX.1.00
+╚━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━""")
+
+a = input ("Enter Your Facebook id :")
+time.sleep(5)
+
+print("\33[1;91mFUCK YOUR \33[1;92m SYSTEM")
+
+
+
+
+print("\33[1;96mKK  🖤🥀   MY LOVE KONIKA 💔  ")
+time.sleep(15)
+
+while True :
+	
+	y = random.randrange(1,9999999999999)
+	print('[ \33[1;92m LOVE YOU KONIKA I MISS YOU 💔🙂 ]  ',y)
